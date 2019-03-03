e0227887dc23f392a9ffecc55d3e9e2a9755c534
TrayanTG <48124699+TrayanTG@users.noreply.github.com>
2019-03-03 06:42:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
 #include<string>
-#include<utility>
 #include<thread>
-#include<chrono>
 
 #include "tcpClient.h"
 
 int main()
 {
-	CtcpClient client("95.111.75.172", 28199);
+	CtcpClient client("127.0.0.1", 28199);
 	if (client.Init() && client.Connect())
 	{
 		std::thread recvThread(&CtcpClient::Recv, &client);recvThread.detach();
